673eee12af7657e77ba8148c4afbe4133d6373c0
Dmitry Zhuk <dmitryzhuk98@gmail.com>
2018-05-17 11:47:59 +0300

    Added license note to DirectionJSONParser.java

--- app/src/main/java/com/github/zhukdi/your_tour/parsers/DirectionsJSONParser.java
+++ app/src/main/java/com/github/zhukdi/your_tour/parsers/DirectionsJSONParser.java
@@ -1,3 +1,20 @@
+/*
+ *   This file is part of Find Your Friend.
+ *
+ *   Find Your Friend is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Find Your Friend is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with Find Your Friend.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.github.zhukdi.your_tour.parsers;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -10,9 +27,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-/**
- * Created by Dmitry on 4/20/2018.
- */
+
 
 public class DirectionsJSONParser {
 
